b88ac91c4d00d9e798bc1747e66d947f1bb21990
Judyzhu1 <52145296+Judyzhu1@users.noreply.github.com>
2020-07-24 10:13:13 -0500

    7/24

--- test/scripts.js
+++ test/scripts.js
@@ -1,6 +1,23 @@
 var search_input = document.querySelector("#search_input");
 
 search_input.addEventListener("keyup", function(e){
+  var span_items = document.querySelectorAll(".table_body .name span");
+  var table_body = document.querySelector(".table_body ul");
+  var search_item = e.target.value.toLowerCase();
+
+ span_items.forEach(function(item){
+   if(item.textContent.toLowerCase().indexOf(search_item) != -1){
+      item.closest("li").style.display = "block";
+   }
+   else{
+     item.closest("li").style.display = "none";
+     }
+ })
+});
+
+var search_input = document.querySelector("#search_input1");
+
+search_input.addEventListener("keyup", function(e){
   var span_items = document.querySelectorAll(".table_body .phone span");
   var table_body = document.querySelector(".table_body ul");
   var search_item = e.target.value.toLowerCase();
@@ -14,6 +31,7 @@ search_input.addEventListener("keyup", function(e){
      }
  })
 });
+
 // var locationSearch = '';
 // document.getElementById('0').addEventListener("click", function(){
 //    var locationSearch = '0';
